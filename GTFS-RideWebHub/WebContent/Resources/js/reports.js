5477df00bda5ca56cff01c2cda0dd2f421ee38d0
Sylvan <hooversy@oregonstate.edu>
2017-10-23 20:21:03 -0700

    pushed pdf libraries and index changes

---
+++ GTFS-RideWebHub/WebContent/Resources/js/reports.js
@@ -0,0 +1,19 @@
+var type = $(document).getElementbyId('reportType')
+var agg = $(document).getElementbyId('reportAggregation')
+var agency = $(document).getElementbyId('reportAgency')
+
+function makeReport(passForm)
+{
+	if(type=='1')
+		{
+		
+		}
+	else if(type=='2')
+		{
+		
+		}
+	else if(type=='3')
+		{
+		
+		}
+}
